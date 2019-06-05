25ae5c241d567155b51689a5e28a90bee3f3c9da
Liesner <liesner@gmail.com>
2019-06-05 13:26:33 +0200

    a

--- src/main/scala/ElasticViewWriter.scala
+++ src/main/scala/ElasticViewWriter.scala
@@ -3,6 +3,6 @@ import org.elasticsearch.spark.sql._
 
 object ElasticViewWriter {
   def writeView[T](view: Dataset[T], viewName : String): Unit = {
-    view.toDF().saveToEs("rtda/" + viewName)
+    view.toDF().saveToEs("rtda" + viewName.toLowerCase + "/" + viewName.toLowerCase)
   }
 }
