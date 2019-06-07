2ff4af51e9b9f585abc9d6b3168a06ecce5b779a
Liesner <liesner@gmail.com>
2019-06-07 05:50:32 +0200

    wip

--- src/main/scala/SurveyProcessing.scala
+++ src/main/scala/SurveyProcessing.scala
@@ -4,6 +4,9 @@ import org.apache.spark.sql.functions._
 
 class SurveyProcessing(surveyDataFrame: DataFrame, spark: SparkSession) {
 
+  def developerCount(): Long = {
+    this.surveyDataFrame.count()
+  }
 
   def createDeveloperOpenSourcePercentageView(): Dataset[DeveloperOpenSourcePercentageView] = {
     import spark.implicits._
@@ -23,38 +26,11 @@ class SurveyProcessing(surveyDataFrame: DataFrame, spark: SparkSession) {
       .as[AgeGenderView]
   }
 
-  def createPercentageDevStudentsView(): Dataset[PercentageDevStudentsView] = {
-    import spark.implicits._
-    surveyDataFrame.groupBy("Student")
-      .count()
-      .withColumn("percentage", (col("count")/ sum("count").over())*100)
-      .orderBy(desc("count")).as[PercentageDevStudentsView]
-  }
+  def createPercentageDevStudentsView(): Dataset[PercentageDevStudentsView] = ???
 
-  def createAvgProfessionalCodingExperienceView(): Dataset[AvgProfessionalCodingExperienceView] = {
-    import spark.implicits._
-    surveyDataFrame.withColumn("DevType", explode(split($"DevType", ";")))
-      .groupBy("DevType")
-      .agg(avg("YearsCodePro").as("avg")).orderBy(desc("avg")).as[AvgProfessionalCodingExperienceView]
-  }
+  def createAvgProfessionalCodingExperienceView(): Dataset[AvgProfessionalCodingExperienceView] = ???
 
-  def createPercentageByEthnicityView(): Dataset[PercentageByEthnicityView] = {
-    import spark.implicits._
-    surveyDataFrame.withColumn("Ethnicity", explode(split($"Ethnicity", ";")))
-      .groupBy("Ethnicity").count()
-      .withColumn("percentage", (col("count")/ sum("count").over())*100)
-      .orderBy(desc("percentage")).as[PercentageByEthnicityView]
-  }
+  def createPercentageByEthnicityView(): Dataset[PercentageByEthnicityView] = ???
 
-  def createPercentageSocialMediaView(): Dataset[PercentageSocialMediaView] = {
-    import spark.implicits._
-    surveyDataFrame.withColumn("SocialMedia", explode(split($"SocialMedia", ";")))
-      .groupBy("SocialMedia").count()
-      .withColumn("percentage", (col("count")/ sum("count").over())*100)
-      .orderBy(desc("percentage")).as[PercentageSocialMediaView]
-  }
-
-  def developerCount(): Long = {
-    this.surveyDataFrame.count()
-  }
+  def createPercentageSocialMediaView(): Dataset[PercentageSocialMediaView] = ???
 }
